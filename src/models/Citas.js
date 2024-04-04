eade4a288bbb878357676dddae31c0518b16ac80
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-04-04 18:42:51 -0500

    modelo citas

---
+++ src/models/Citas.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const DateSchema = mongoose.Schema(
+  {
+    dia: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    inicio: {
+      type: Date,
+      required: true,
+    },
+    fin: {
+      type: Date,
+      required: true,
+    },
+    comentarios: {
+      type: String,
+      required: false,
+    },
+    receta: {
+      type: String,
+      required: false,
+    },
+    idPaciente: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuarios",
+      required: true,
+    },
+    idDoctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuarios",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CitaModelo = mongoose.model("Citas", DateSchema);
+
+export default CitaModelo;
