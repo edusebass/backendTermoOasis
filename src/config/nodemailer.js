0025179644a127551e23caf5a33572bc27d0972e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-04-04 18:42:32 -0500

    add envio a paciente cita

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -49,29 +49,51 @@ const emailMailRecuperarContraseña = async (userMail, token) =>{
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
 }
 
-
+const enviarEmailCita = async (datos) => {
+    const { nombrePaciente, email, especialistemail, code, cita } = datos;
+    console.log(email);
+  
+    let info = await transporter.sendMail({
+      //the client email
+      to: [`${email}`, `${especialistemail}`],
+      //sendGrid sender id
+      from: "drbariatrico250@gmail.com",
+      subject: `Saludos ${nombrePaciente} ¡Te han asignado una fecha para una cita!`,
+      text: "Notificación de cita",
+      html: `<p> Te han asignado una fecha el ${cita.inicio} </p>
+            <p> Recuerda asistir puntualmente</p>
+            <p> Además, recuerda que solo puedes cancelar tucita hasta 1 hora antes</p>
+            
+            <p>Si tu no solicitaste este servicio, puedes ignorar este email</p>
+            `,
+      code
+    })
+    
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+  };
 
 // send mail with defined transport object
-const sendMailToPaciente = async(userMail, password)=>{
-    let info = await transporter.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Correo de bienvenida",
-    html: `
-    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
-    <hr>
-    <p>Contraseña de acceso: ${password}</p>
-    <a href=${process.env.URL_BACKEND}paciente/login>Clic para iniciar sesión</a>
-    <hr>
-    <footer>Grandote te da la Bienvenida!</footer>
-    `
-    });
-    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
-}
+// const sendMailToPaciente = async(userMail, password)=>{
+//     let info = await transporter.sendMail({
+//     from: 'admin@vet.com',
+//     to: userMail,
+//     subject: "Correo de bienvenida",
+//     html: `
+//     <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+//     <hr>
+//     <p>Contraseña de acceso: ${password}</p>
+//     <a href=${process.env.URL_BACKEND}paciente/login>Clic para iniciar sesión</a>
+//     <hr>
+//     <footer>Grandote te da la Bienvenida!</footer>
+//     `
+//     });
+//     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+// }
 
 
 export {
     sendMailToUser,
     emailMailRecuperarContraseña,
-    sendMailToPaciente
+    enviarEmailCita
+    // sendMailToPaciente
 }
