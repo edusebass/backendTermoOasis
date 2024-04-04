6b93fbd17afd89da78732abcc12d406ae36122f3
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-04-04 18:48:25 -0500

    add autenticacion por token

--- src/middlewares/autenticacion.js
+++ src/middlewares/autenticacion.js
@@ -1,26 +1,14 @@
-//Importacion
 import jwt from 'jsonwebtoken'
-import Especialista from '../models/Especialista.js'
-import Paciente from '../models/Paciente.js'
+import Usuario from '../models/Usuario.js'
 
-//Metodo para proteger rutas
 const verificarAutenticacion = async (req,res,next)=>{
-    //vvalidacion si se esta enviando el token
-    if(!req.headers.authorization) return res.status(404).json({msg:"Lo sentimos, debes proprocionar un token"})  
-    // desestructurar el token  
+
+if(!req.headers.authorization) return res.status(404).json({msg:"Lo sentimos, debes proprocionar un token"})    
     const {authorization} = req.headers
-    // capturar errores del try-catch
     try {
-        //verificar el token recupearado con el almacenado
         const {id,rol} = jwt.verify(authorization.split(' ')[1],process.env.JWT_SECRET)
-        // verificar el rol
-        if (rol==="especialista"){
-            //Obtener el usuario
-            req.EspecialistaBDD = await Especialista.findById(id).lean().select("-password")
-            //continue el proceso
-            next()
-        }else{
-            req.pacienteBDD = await Paciente.findById(id).lean().select("-password")
+        if (rol==="usuario"){
+            req.usuarioBDD = await Usuario.findById(id).lean().select("-password")
             next()
         }
     } catch (error) {
