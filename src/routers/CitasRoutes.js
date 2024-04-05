ed0b44c761b1c7fa4aa92d17b738031f24c52537
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-04-05 15:20:26 -0500

    add enpoint cancelar cita

--- src/routers/CitasRoutes.js
+++ src/routers/CitasRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import verificarAutenticacion from '../middlewares/autenticacion.js'
 import{
+    cancelarCita,
     crearCita
 } from "../controllers/CitaController.js";
 const citaRouter = Router()
 
-citaRouter.post("/registrar", verificarAutenticacion, crearCita) //endpoint para el login
+citaRouter.post("/registrar", verificarAutenticacion, crearCita) 
+citaRouter.post("/cancelar/:id", verificarAutenticacion, cancelarCita) 
 
 
 export default citaRouter
