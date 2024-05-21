cfb5a926cb325f06c9a8006742ed669e2b6d5707
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-05-21 10:05:43 -0500

    add endpoitn detalle paciente

--- src/routers/UsuarioRoutes.js
+++ src/routers/UsuarioRoutes.js
@@ -9,19 +9,24 @@ import{
     perfil,
     comprobarTokenContraseña,
     recuperarContraseñaMovil,
+    detallePaciente,
 } from "../controllers/UsuarioController.js";
 const usuarioRouter = Router()
 
 usuarioRouter.post("/login", login) 
 usuarioRouter.post("/registro", registro) 
 usuarioRouter.post("/recuperar-password", recuperarContraseña)
-usuarioRouter.post("/recuperar-password-movil", recuperarContraseñaMovil) 
 usuarioRouter.get("/recuperar-password/:token", comprobarTokenContraseña);
 usuarioRouter.post("/nueva-password/:token", nuevaContraseña)
+
+usuarioRouter.post("/recuperar-password-movil", recuperarContraseñaMovil) 
 
 
 
 usuarioRouter.get("/listar-pacientes", verificarAutenticacion, obtenerPacientes)
 usuarioRouter.get("/perfil", verificarAutenticacion, perfil)
+
+
+usuarioRouter.get("/detallePaciente/:id", verificarAutenticacion, detallePaciente)
 
 export default usuarioRouter
