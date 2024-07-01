aac022805e995b9844c1fe2ee0b715817bf69b64
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-07-01 14:37:49 -0500

    prueba docs deploy

--- src/server.js
+++ src/server.js
@@ -30,7 +30,13 @@ app.use('/api/registroMedico', registroRouter); // Endpoints para registros méd
 const CSS_URL ="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 // Middleware de Swagger
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api-docs', swaggerUI.serve,
+    swaggerUI.setup(swaggerSpec, {
+      customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      customCssUrl: CSS_URL,
+    },
+));
 
 // Manejo de una ruta que no sea encontrada
 app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"));
