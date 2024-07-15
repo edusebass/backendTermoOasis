28a796ba85e6657905c3b4fb1c988dbb332c87b4
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-07-14 21:30:49 -0500

    feat: update docu cambio a id de la cita

--- src/routes/RegistroRoutes.js
+++ src/routes/RegistroRoutes.js
@@ -91,7 +91,7 @@ registroRouter.post("/crear", verificarAutenticacion, crearRegistro);
  *         required: true
  *         schema:
  *           type: string
- *         description: ID del registro medico
+ *         description: ID de la cita medica
  *       - in: body
  *         name: registro
  *         description: Datos del registro médico a actualizar
