9780b707191ec4a82569d6e3a56a6f595e43ffc7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-08-04 22:08:19 -0500

    feat: add local server docs

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,31 +1,40 @@
-import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerJSDoc from "swagger-jsdoc";
 
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API Documentacion TERMO OASIS',
-            version: '1.0.0',
-            description: 'Documentacion para los endpoints',
-        },
-        servers: [{ url: 'https://backend-termo-oasis.vercel.app', description: 'Production server' }],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                },
-            },
-        },
-        security: [
-            {
-                bearerAuth: [],
-            },
-        ],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentacion TERMO OASIS",
+      version: "1.0.0",
+      description: "Documentacion para los endpoints",
     },
+    servers: [
+      {
+        url: "https://backend-termo-oasis.vercel.app",
+        description: "Production server",
+      },
+      {
+        url: "http://localhost:3001",
+        description: "Local server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
 
-    apis: ["./src/**/*.js"],
+  apis: ["./src/**/*.js"],
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
