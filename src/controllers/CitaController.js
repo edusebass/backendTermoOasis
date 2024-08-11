2e73a788e760a5c8eaada316c3b7dd576847532f
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-08-11 17:07:10 -0500

    feat: update correcion que pidio el forntend

--- src/controllers/CitaController.js
+++ src/controllers/CitaController.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 import CitaModelo from "../models/Citas.js";
 import UsuarioModelo from "../models/Usuario.js";
-import { emailActualizarCita, emailCancelarCita, emailRecordatorioCita, enviarEmailCita} from "../config/nodemailer.js";
-import cron from 'node-cron';
+import {
+  emailActualizarCita,
+  emailCancelarCita,
+  emailRecordatorioCita,
+  enviarEmailCita,
+} from "../config/nodemailer.js";
+import cron from "node-cron";
 
 const crearCita = async (req, res) => {
   const { idPaciente, idDoctor, start, end, comentarios } = req.body;
 
-  const isSecre = req.headers['issecre'] === 'true';
-  if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+  const isSecre = req.headers["issecre"] === "true";
+  if (Object.values(req.body).includes(""))
+    return res
+      .status(400)
+      .json({ msg: "Lo sentimos, debes llenar todos los campos" });
 
   // Verificar los permisos de acceso
   if (!isSecre) {
@@ -16,19 +24,33 @@ const crearCita = async (req, res) => {
   }
 
   if (Object.values(req.body).includes("")) {
-    return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos" });
+    return res
+      .status(400)
+      .json({ msg: "Lo sentimos, debes llenar todos los campos" });
   }
 
   if (!idPaciente || !idDoctor || !start || !end || comentarios === undefined) {
-    return res.status(400).json({ msg: "Lo sentimos, debes poner todos los campos en el body", status: false });
+    return res.status(400).json({
+      msg: "Lo sentimos, debes poner todos los campos en el body",
+      status: false,
+    });
   }
 
   if (comentarios.length > 50) {
-    return res.status(400).json({ msg: "El comentario no debe exceder los 30 caracteres", status: false });
+    return res.status(400).json({
+      msg: "El comentario no debe exceder los 30 caracteres",
+      status: false,
+    });
   }
 
-  if (!mongoose.Types.ObjectId.isValid(idPaciente) || !mongoose.Types.ObjectId.isValid(idDoctor)) {
-    return res.status(400).json({ msg: "El ID no es válido no pertenece a mongoDB", status: false });
+  if (
+    !mongoose.Types.ObjectId.isValid(idPaciente) ||
+    !mongoose.Types.ObjectId.isValid(idDoctor)
+  ) {
+    return res.status(400).json({
+      msg: "El ID no es válido no pertenece a mongoDB",
+      status: false,
+    });
   }
 
   try {
@@ -42,10 +64,14 @@ const crearCita = async (req, res) => {
     });
 
     if (!existePaciente) {
-      return res.status(400).json({ msg: "Paciente no se encuentra registrado", status: false });
+      return res
+        .status(400)
+        .json({ msg: "Paciente no se encuentra registrado", status: false });
     }
     if (!existeDoctor) {
-      return res.status(400).json({ msg: "Doctor no se encuentra registrado", status: false });
+      return res
+        .status(400)
+        .json({ msg: "Doctor no se encuentra registrado", status: false });
     }
 
     // Verificar el formato de fecha
@@ -53,54 +79,85 @@ const crearCita = async (req, res) => {
     const finInput = new Date(end);
 
     if (inicioInput < new Date() || finInput < new Date()) {
-      return res.status(400).json({ msg: "No puedes agendar citas en el pasado", status: false });
+      return res
+        .status(400)
+        .json({ msg: "No puedes agendar citas en el pasado", status: false });
     }
 
     if (inicioInput.getTime() === finInput.getTime()) {
-      return res.status(400).json({ msg: "Debes elegir un horario de inicio y fin diferente", status: false });
+      return res.status(400).json({
+        msg: "Debes elegir un horario de inicio y fin diferente",
+        status: false,
+      });
     }
 
-    const diferenciaHoras = (finInput.getTime() - inicioInput.getTime()) / (1000 * 60 * 60);
+    const diferenciaHoras =
+      (finInput.getTime() - inicioInput.getTime()) / (1000 * 60 * 60);
     if (diferenciaHoras < 1) {
-      return res.status(400).json({ msg: "La diferencia entre la hora de inicio y fin debe ser de al menos una hora", status: false });
+      return res.status(400).json({
+        msg: "La diferencia entre la hora de inicio y fin debe ser de al menos una hora",
+        status: false,
+      });
     }
 
     if (diferenciaHoras !== 1) {
-      return res.status(400).json({ msg: "La cita debe durar exactamente una hora", status: false });
+      return res.status(400).json({
+        msg: "La cita debe durar exactamente una hora",
+        status: false,
+      });
     }
 
     const startDateStr = start;
     const endDateStr = end;
 
     if (startDateStr.substr(0, 10) !== endDateStr.substr(0, 10)) {
-      return res.status(400).json({ msg: "El inicio y fin de la cita deben ser en el mismo día", status: false });
+      return res.status(400).json({
+        msg: "El inicio y fin de la cita deben ser en el mismo día",
+        status: false,
+      });
     }
 
     const currentYear = new Date().getFullYear();
-    if (new Date(startDateStr).getFullYear() !== currentYear || new Date(endDateStr).getFullYear() !== currentYear) {
-      return res.status(400).json({ msg: "El inicio y fin de la cita deben ser en el mismo año actual", status: false });
+    if (
+      new Date(startDateStr).getFullYear() !== currentYear ||
+      new Date(endDateStr).getFullYear() !== currentYear
+    ) {
+      return res.status(400).json({
+        msg: "El inicio y fin de la cita deben ser en el mismo año actual",
+        status: false,
+      });
     }
 
+    // Verificar si hay conflictos con otras citas, permitiendo un minuto de superposición
     // Verificar si hay conflictos con otras citas, permitiendo un minuto de superposición
     const existingCita = await CitaModelo.findOne({
       $or: [
         {
-          start: { $lt: new Date(new Date(endDateStr).getTime() + 60000).toISOString() }, // Permitir un minuto después de end
-          end: { $gt: new Date(new Date(startDateStr).getTime() - 60000).toISOString() } // Permitir un minuto antes de start
+          start: {
+            $lt: new Date(new Date(endDateStr).getTime() + 60000).toISOString(),
+          }, // Permitir un minuto después de end
+          end: {
+            $gt: new Date(
+              new Date(startDateStr).getTime() - 60000
+            ).toISOString(),
+          }, // Permitir un minuto antes de start
         },
         {
           start: { $lte: startDateStr },
-          end: { $gte: endDateStr }
+          end: { $gte: endDateStr },
         },
         {
           start: { $lte: startDateStr },
-          end: { $gte: startDateStr }
-        }
-      ]
+          end: { $gte: startDateStr },
+        },
+      ],
+      isCancelado: false, // Solo considera citas que no estén canceladas
     });
 
     if (existingCita) {
-      return res.status(400).json({ msg: "Ya existe una cita en ese horario!", status: false });
+      return res
+        .status(400)
+        .json({ msg: "Ya existe una cita en ese horario!", status: false });
     }
 
     // Guardar la nueva cita
@@ -127,7 +184,7 @@ const crearCita = async (req, res) => {
       nombrePaciente: existePaciente.nombre,
       email: existePaciente.email,
       emailDoctor: existeDoctor.email,
-      cita: cita
+      cita: cita,
     });
 
     // Obtener la cita completa con las referencias pobladas
@@ -136,24 +193,28 @@ const crearCita = async (req, res) => {
       .populate("idPaciente")
       .populate("registroMedico");
 
-    res.status(200).json({ msg: "Cita agendada correctamente", status: true, data: fullCita });
+    res.status(200).json({
+      msg: "Cita agendada correctamente",
+      status: true,
+      data: fullCita,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ msg: error.message, status: false });
   }
 };
 
-
 const editarCita = async (req, res) => {
   const { id } = req.params;
-  const isSecre = req.headers['issecre'] === 'true';
-  console.log(id)
-  
-  try {
-    
+  const isSecre = req.headers["issecre"] === "true";
+  console.log(id);
 
+  try {
     const cita = await CitaModelo.findById(id);
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    if (Object.values(req.body).includes(""))
+      return res
+        .status(400)
+        .json({ msg: "Lo sentimos, debes llenar todos los campos" });
 
     if (!isSecre) {
       return res.status(403).json({ msg: "Acceso denegado", status: false });
@@ -166,16 +227,24 @@ const editarCita = async (req, res) => {
 
     const { start: startISO, end: endISO, comentarios, isCancelado } = req.body;
 
-    if ( !startISO || !endISO || !isCancelado || comentarios === undefined) {
-      return res.status(400).json({ msg: "Lo sentimos, faltan campos", status: false });
+    if (!startISO || !endISO || comentarios === undefined) {
+      return res
+        .status(400)
+        .json({ msg: "Lo sentimos, faltan campos", status: false });
     }
-  
+
     if (comentarios.length > 50) {
-      return res.status(400).json({ msg: "El comentario no debe exceder los 30 caracteres", status: false });
+      return res.status(400).json({
+        msg: "El comentario no debe exceder los 30 caracteres",
+        status: false,
+      });
     }
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ msg: "El ID no es válido no pertenece a mongoDB", status: false });
+      return res.status(400).json({
+        msg: "El ID no es válido no pertenece a mongoDB",
+        status: false,
+      });
     }
 
     let inicioInput = new Date(startISO);
@@ -189,26 +258,39 @@ const editarCita = async (req, res) => {
 
     // Verificar si las fechas de inicio y fin son iguales
     if (inicioInput.getTime() === finInput.getTime()) {
-      const error = new Error("Debes elegir un horario de inicio y fin diferente");
+      const error = new Error(
+        "Debes elegir un horario de inicio y fin diferente"
+      );
       return res.status(400).json({ msg: error.message, status: false });
     }
 
     // Verificar si la diferencia entre las fechas es exactamente una hora
-    const diferenciaHoras = (finInput.getTime() - inicioInput.getTime()) / (1000 * 60 * 60);
+    const diferenciaHoras =
+      (finInput.getTime() - inicioInput.getTime()) / (1000 * 60 * 60);
     if (diferenciaHoras !== 1) {
       const error = new Error("La cita debe durar exactamente una hora");
       return res.status(400).json({ msg: error.message, status: false });
     }
 
     // Verificar si start y end están en el mismo día
-    if (inicioInput.toISOString().substr(0, 10) !== finInput.toISOString().substr(0, 10)) {
-      const error = new Error("El inicio y fin de la cita deben ser en el mismo día");
+    if (
+      inicioInput.toISOString().substr(0, 10) !==
+      finInput.toISOString().substr(0, 10)
+    ) {
+      const error = new Error(
+        "El inicio y fin de la cita deben ser en el mismo día"
+      );
       return res.status(400).json({ msg: error.message, status: false });
     }
 
     // Verificar si start y end están en el mismo año
-    if (inicioInput.getFullYear() !== new Date().getFullYear() || finInput.getFullYear() !== new Date().getFullYear()) {
-      const error = new Error("El inicio y fin de la cita deben ser en el mismo año actual");
+    if (
+      inicioInput.getFullYear() !== new Date().getFullYear() ||
+      finInput.getFullYear() !== new Date().getFullYear()
+    ) {
+      const error = new Error(
+        "El inicio y fin de la cita deben ser en el mismo año actual"
+      );
       return res.status(400).json({ msg: error.message, status: false });
     }
 
@@ -218,19 +300,19 @@ const editarCita = async (req, res) => {
         {
           _id: { $ne: id },
           start: { $lt: endISO },
-          end: { $gt: startISO }
+          end: { $gt: startISO },
         },
         {
           _id: { $ne: id },
           start: { $lte: startISO },
-          end: { $gte: endISO }
+          end: { $gte: endISO },
         },
         {
           _id: { $ne: id },
           start: { $lte: startISO },
-          end: { $gte: startISO }
-        }
-      ]
+          end: { $gte: startISO },
+        },
+      ],
     });
 
     if (existingCita) {
@@ -253,7 +335,7 @@ const editarCita = async (req, res) => {
     });
     const existDoctor = await UsuarioModelo.findOne({
       _id: cita.idDoctor,
-      isDoctor: true
+      isDoctor: true,
     });
 
     if (!existPaciente) {
@@ -272,38 +354,42 @@ const editarCita = async (req, res) => {
       firstname: existPaciente.nombre,
       email: existPaciente.email,
       especialistemail: existDoctor.email,
-      cita
+      cita,
     });
-    res.status(200).json({ response: "Cita actualizada exitosamente", msg: citastored, status: true });
-    } catch {
-      res.status(400).json({ msg: error.message, status: false });
-    }
- 
+    res.status(200).json({
+      response: "Cita actualizada exitosamente",
+      msg: citastored,
+      status: true,
+    });
+  } catch {
+    res.status(400).json({ msg: error.message, status: false });
+  }
 };
-
 
 const mostrarCitasPorPaciente = async (req, res) => {
   const { id } = req.params;
-  const isSecre = req.headers['issecre'] === 'true';
-  const isDoctor = req.headers['isdoctor'] === 'true';
-  const isPaciente = req.headers['ispaciente'] === 'true';
+  const isSecre = req.headers["issecre"] === "true";
+  const isDoctor = req.headers["isdoctor"] === "true";
+  const isPaciente = req.headers["ispaciente"] === "true";
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ msg: "El ID no es válido no pertenece a mongoDB", status: false });
+    return res.status(400).json({
+      msg: "El ID no es válido no pertenece a mongoDB",
+      status: false,
+    });
   }
 
   if (!isSecre && !isDoctor && !isPaciente) {
     return res.status(403).json({ msg: "Acceso denegado", status: false });
   }
-  console.log(id)
-  console.log(typeof id)
+  console.log(id);
+  console.log(typeof id);
   try {
-      const dates = await CitaModelo.find({
-        idPaciente: id,
-      }).populate("idDoctor")
-      
-      res.status(200).json({ data: dates, status: true });
-    
+    const dates = await CitaModelo.find({
+      idPaciente: id,
+    }).populate("idDoctor");
+
+    res.status(200).json({ data: dates, status: true });
   } catch (error) {
     res.status(400).json({ msg: error.message, status: false });
   }
@@ -311,19 +397,25 @@ const mostrarCitasPorPaciente = async (req, res) => {
 
 const cancelarCita = async (req, res) => {
   const { id } = req.params;
-  
-  const isSecre = req.headers['issecre'] === 'true';
-  const isPatient = req.headers['ispaciente'] === 'true';
-  
+
+  const isSecre = req.headers["issecre"] === "true";
+  const isPatient = req.headers["ispaciente"] === "true";
+
   // Verificar los permisos de acceso
   if (!isSecre && !isPatient) {
     return res.status(403).json({ msg: "Acceso denegado", status: false });
   }
 
-  if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+  if (Object.values(req.body).includes(""))
+    return res
+      .status(400)
+      .json({ msg: "Lo sentimos, debes llenar todos los campos" });
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ msg: "El ID no es válido no pertenece a mongoDB", status: false });
+    return res.status(400).json({
+      msg: "El ID no es válido no pertenece a mongoDB",
+      status: false,
+    });
   }
 
   try {
@@ -338,10 +430,12 @@ const cancelarCita = async (req, res) => {
 
     const ahora = new Date();
     const inicioCita = new Date(cita.start);
-    console.log(inicioCita)
+    console.log(inicioCita);
     const diferenciaTiempo = inicioCita.getTime() - ahora.getTime();
     if (diferenciaTiempo < 86400000) {
-      return res.status(400).json({ msg: "No puedes cancelar la cita con menos de 24 horas de antelación" });
+      return res.status(400).json({
+        msg: "No puedes cancelar la cita con menos de 24 horas de antelación",
+      });
     }
 
     emailCancelarCita({
@@ -357,19 +451,19 @@ const cancelarCita = async (req, res) => {
 };
 
 const mostrarCitas = async (req, res) => {
-  const isSecre = req.headers['issecre'] === 'true';
-  const isDoctor = req.headers['isdoctor'] === 'true';
-  const isPaciente = req.headers['ispaciente'] === 'true';
+  const isSecre = req.headers["issecre"] === "true";
+  const isDoctor = req.headers["isdoctor"] === "true";
+  const isPaciente = req.headers["ispaciente"] === "true";
 
   if (!isSecre && !isDoctor && !isPaciente) {
     return res.status(403).json({ msg: "Acceso denegado", status: false });
   }
-  
+
   try {
     const citas = await CitaModelo.find().populate({
-    path: 'idPaciente',
-    select: 'nombre apellido cedula'
-  })
+      path: "idPaciente",
+      select: "nombre apellido cedula",
+    });
 
     citas.sort((date1, date2) => date2.updatedAt - date1.updatedAt);
 
@@ -381,28 +475,32 @@ const mostrarCitas = async (req, res) => {
 
 const mostrarCitaID = async (req, res) => {
   const { id } = req.params;
-  
-  const isSecre = req.headers['issecre'] === 'true';
-  const isDoctor = req.headers['isdoctor'] === 'true';
-  const isPaciente = req.headers['ispaciente'] === 'true';
 
-  console.log(isSecre, isDoctor, isPaciente)
+  const isSecre = req.headers["issecre"] === "true";
+  const isDoctor = req.headers["isdoctor"] === "true";
+  const isPaciente = req.headers["ispaciente"] === "true";
+
+  console.log(isSecre, isDoctor, isPaciente);
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ msg: "El ID no es válido no pertenece a mongoDB", status: false });
+    return res.status(400).json({
+      msg: "El ID no es válido no pertenece a mongoDB",
+      status: false,
+    });
   }
 
-  
   if (!isSecre && !isDoctor && !isPaciente) {
     return res.status(403).json({ msg: "Acceso denegado", status: false });
   }
 
   try {
     // obtiene la cita por el id
-    const citaExist = await CitaModelo.findById(id).populate("idPaciente").populate({
-      path: "idDoctor",
-      select: "nombre apellido"
-    });;
+    const citaExist = await CitaModelo.findById(id)
+      .populate("idPaciente")
+      .populate({
+        path: "idDoctor",
+        select: "nombre apellido",
+      });
 
     // si no existe salta un error
     if (!citaExist) {
@@ -422,8 +520,8 @@ const verificarCitasProximas = async () => {
   try {
     const citasProximas = await CitaModelo.find({
       start: { $gte: doceHorasAntes.toISOString(), $lte: ahora.toISOString() },
-      recordatory: false // Filtrar solo las citas donde recordatory sea false
-    }).populate('idPaciente');
+      recordatory: false, // Filtrar solo las citas donde recordatory sea false
+    }).populate("idPaciente");
 
     for (const cita of citasProximas) {
       await emailRecordatorioCita(cita.idPaciente.email, cita.start);
@@ -435,11 +533,11 @@ const verificarCitasProximas = async () => {
 
     console.log("Revisando si hay citas proximas...");
   } catch (error) {
-    console.error('Error al verificar las citas próximas:', error);
+    console.error("Error al verificar las citas próximas:", error);
   }
 };
 
-cron.schedule('*/60 * * * * *', verificarCitasProximas); // Ejecutar cada 30 segundos
+cron.schedule("*/60 * * * * *", verificarCitasProximas); // Ejecutar cada 30 segundos
 
 export {
   crearCita,
@@ -447,5 +545,5 @@ export {
   cancelarCita,
   mostrarCitas,
   mostrarCitaID,
-  mostrarCitasPorPaciente
-}
+  mostrarCitasPorPaciente,
+};
